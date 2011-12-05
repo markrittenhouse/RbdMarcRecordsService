d522130b66df3f4c0e5112f0104e1507502324dc
scottscheider <scottscheider@technotects.com>
2011-12-05 19:15:24 +0000

    Updated to test MARCEngine DLL deployment on technoserv04

---
+++ MarcRecordServiceSite/MarcRecordServiceSite/Infrastructure/NHibernate/Mappings/MarcRecordFileMap.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using FluentNHibernate.Mapping;
+using MarcRecordServiceSite.Infrastructure.NHibernate.Entities;
+
+namespace MarcRecordServiceSite.Infrastructure.NHibernate.Mappings
+{
+	public sealed class MarcRecordFileMap : ClassMap<MarcRecordFile>
+	{
+		public MarcRecordFileMap()
+		{
+			Table("MarcRecordFile");
+
+			// mrf.marcRecordFileId, mrf.marcRecordProviderId, mrf.marcRecordFileTypeId, mrf.fileData
+			Id(x => x.Id).Column("marcRecordFileId");
+			Map(x => x.MarcRecordFileTypeId).Column("marcRecordFileTypeId");
+			Map(x => x.FileData).Column("fileData");
+			References<MarcRecordProvider>(x => x.Provider).Column("marcRecordProviderId");
+		}
+	}
+}
