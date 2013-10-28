d2e2d21e3189d321635204053a747d75bb45fa5a
kenhaberle@technotects.com <kenhaberle@technotects.com@6deda771-54a4-4b00-9666-1ce3f79db35f>
2013-10-28 15:37:13 +0000

    Made changes to the SQL query to speed up the request time.

---
+++ trunk/MarcRecordServiceSite/MarcRecordServiceSite/Infrastructure/NHibernate/Mappings/DailyMarcRecordFileMap.cs
@@ -0,0 +1,20 @@
+﻿using FluentNHibernate.Mapping;
+using MarcRecordServiceSite.Infrastructure.NHibernate.Entities;
+
+namespace MarcRecordServiceSite.Infrastructure.NHibernate.Mappings
+{
+    public sealed class DailyMarcRecordFileMap : ClassMap<DailyMarcRecordFile>
+	{
+        public DailyMarcRecordFileMap()
+		{
+            Table("DailyMarcRecordFile");
+
+            // dailyMarcRecordFileId, isbn10, isbn13, sku, fileData
+            Id(x => x.Id).Column("dailyMarcRecordFileId");
+            Map(x => x.Isbn10).Column("isbn10");
+            Map(x => x.Isbn13).Column("isbn13");
+            Map(x => x.Sku).Column("sku");
+            Map(x => x.MarcFile).Column("fileData");
+		}
+	}
+}
