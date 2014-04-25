57b45f911bbd582047e8191aae07683e57884907
KenHaberle <kenhaberle@technotects.com>
2014-04-25 17:57:42 +0000

    checking in older changes that have been in production for awhile.

--- MarcRecordServiceApp/MarcRecordServiceApp/Core/DataAccess/Entities/Product.cs
+++ MarcRecordServiceApp/MarcRecordServiceApp/Core/DataAccess/Entities/Product.cs
@@ -1,7 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
 using MarcRecordServiceApp.Core.DataAccess.Entities.Base;
-using MarcRecordServiceApp.Core.DataAccess.Entities.ProductStatus;
 
 namespace MarcRecordServiceApp.Core.DataAccess.Entities
 {
@@ -10,77 +8,23 @@ namespace MarcRecordServiceApp.Core.DataAccess.Entities
         public int Id { get; set; }
         public string Sku { get; set; }
         public string Isbn10 { get; set; }
-        //public string Isbn10Formatted { get; set; }
         public string Isbn13 { get; set; }
-        public string Title { get; set; }
-        //public string SubTitle { get; set; }
-        public string Authors { get; set; }
-       // public string FirstAuthorLastName { get; set; }
+        public string EIsbn { get; set; }
 
-		public Category Category { get; set; }
-		public Publisher Publisher { get; set; }
-		public IProductStatus Status { get; set; }
-        public string Affiliation { get; set; }
+        public string Title { get; set; }
+        public string SubTitle { get; set; }
+        public string Authors { get; set; }
+        public string FirstAuthorLastName { get; set; }
+
+		
         public int Copyright { get; set; }
         public string PublicationDate { get; set; }
         public string Format { get; set; }
         public string Edition { get; set; }
 
-        public string Pages { get; set; }
-        public decimal Weight { get; set; }
-        public int Length { get; set; }
-        public int Width { get; set; }
-        public int Thickness { get; set; }
-        public int CartonQuantity { get; set; }
-        public string SeriesName { get; set; }
-        public string LCCallNumber { get; set; }
+        public string CategoryName { get; set; }
+        public string PublisherName { get; set; }		
 
-        public string NLM { get; set; }
-        public string AnnouncedEdition { get; set; }
-        public int AnnouncedDate { get; set; }
-        public string NewEditionIsbn { get; set; }
-        public string PreviousEditionIsbn { get; set; }
-        public string AlternateIsbn { get; set; }
-        public string BrandonHillCodes { get; set; }
-        public string PriceGroup { get; set; }
-
-        public string VendorNumber { get; set; }
-        public string VendorInfo { get; set; }
-        public bool IsReturnable { get; set; }
-
-        public bool IsGsa { get; set; }
-        public string ProductType { get; set; }
-        public string AudiencePrimary { get; set; }
-        public string AudienceSecondary { get; set; }
-
-        public string Imprint { get; set; }
-        public short DoodyRating { get; set; }
-        //public string BisacCode { get; set; }
-        //public string BisacDescription { get; set; }
-        public string Synopsis { get; set; }
-        public string PublisherPrelude { get; set; }
-
-        public decimal[] Prices { get; set; }
-
-        public int QuantityOnHand { get; set; }
-        public int QuantityOnOrder { get; set; }
-        public bool InStockOverride { get; set; }
-
-        public int R2ResourceId { get; set; }
-
-        public string Image { get; set; }
-
-        public string CountryCode { get; set; }
-        public string EIsbn { get; set; }
-        public bool IsAvailableForSale { get; set; }
-        public string AvailabilityCode { get; set; }
-
-        public string TocFilename { get; set; }
-
-		//public DoodyCoreTitleScore[] DoodyCoreTitleScores { get; set; }
-
-		//public BackOrderData BackOrder { get; set; }
-        public int OrderHistoryCount { get; set; }
 
         /// <summary>
         /// Calculates publication year based on the value of the copyright field and the publication date field
@@ -120,176 +64,6 @@ namespace MarcRecordServiceApp.Core.DataAccess.Entities
         }
 
 
-        ///// <summary>
-        ///// 
-        ///// </summary>
-        //public string TitleSubTitleSpacer
-        //{
-        //    get
-        //    {
-        //        return string.IsNullOrEmpty(SubTitle) ? "" : ":";
-        //    }
-        //}
-
-		/// <summary>
-		/// 
-		/// </summary>
-		public string CategoryName
-		{
-			get
-			{
-				if ((null != Category) && (!string.IsNullOrEmpty(Category.Name)))
-				{
-					return Category.Name;
-				}
-				return "";
-			}
-		}
-
-        ///// <summary>
-        ///// 
-        ///// </summary>
-        //public string ProductStatus
-        //{
-        //    get
-        //    {
-        //        if ((null != Status) && (!string.IsNullOrEmpty(Status.Description)))
-        //        {
-        //            return Status.Description;
-        //        }
-        //        return "";
-        //    }
-        //}
-
-		/// <summary>
-		/// 
-		/// </summary>
-		public string PublisherName
-		{
-			get
-			{
-				if ((null != Publisher) && (Publisher.Id > 0) && (string.IsNullOrEmpty(Publisher.Name)))
-				{
-					return Publisher.Name;
-				}
-				return PublisherPrelude;
-			}
-		}
-
-		///// <summary>
-		///// 
-		///// </summary>
-		//public string QuantityOnHandText
-		//{
-		//    get
-		//    {
-		//        //QTY OH Business Rule:
-		//        //  If QTY OH = less than 0, display 0
-
-		//        //Status	Display
-		//        //ACT	    - QTY OH XXXX
-		//        //          - If qty = less than 0, display 0
-		//        //          - If QTY OH = 0 display new line item: QTY on order from publisher = XXXX	
-		//        //NYP	    - QTY OH = O 
-		//        //          - Add Line Item: QTY on order from publisher = XXXX	
-		//        //OP        - QTY OH XXXX
-		//        //          - If qty = less than 0, display 0	
-		//        //OS	    - QTY OH = XXX
-		//        //          - If qty = less than 0, display 0
-		//        //          - Add Text: Reprint Pending 	
-		//        //OSI	    - QTY OH = XXX
-		//        //          - If qty = less than 0, display 0
-		//        //          - Add Text: No Reprint Date	
-		//        //PC	    - QTY OH = XXX
-		//        //          - If qty = less than 0, display 0	
-		//        //PI	    - QTY OH = XXX
-		//        //          - If qty = less than 0, display 0	
-		//        //POD	    - QTY OH = XXX
-		//        //          - If qty = less than 0, display 0	
-
-		//        //Exceptions
-		//        //All designated as QTY OH = In Stock
-		//        //  - LWW titles
-		//        //  - LWW Packages
-		//        //  - Drop Ship Only Titles
-		//        //Log.DebugFormat("Status.Code: {0}, Status.Id: {1}, QuantityOnHand: {2}", Status.Code, Status.Id, QuantityOnHand);
-		//        //Log.DebugFormat("InStockOverride: {0}, Format: {1}", InStockOverride, Format);
-		//        int quantityOnHand = (QuantityOnHand < 0) ? 0 : QuantityOnHand;
-
-		//        string returnValue = null;
-		//        //if ((productStatusCodes != ProductStatusCodes.NYP) && ((InStockOverride) || (Format.Equals("LWW Text Package"))))
-		//        //if (((InStockOverride) || (Format.Equals("LWW Text Package")) || AvailabilityCode.Equals("D")) && (quantityOnHand <= 0))
-		//        if ((quantityOnHand <= 0) &&
-		//            ((InStockOverride) || 
-		//             ((null != Format) && (Format.Equals("LWW Text Package"))) ||
-		//             ((null != AvailabilityCode) && (AvailabilityCode.Equals("D")))))
-		//        {
-		//            returnValue = "In Stock";
-		//        }
-		//        else
-		//        {
-		//            returnValue = Status.GetQuantityOnHandText(quantityOnHand);
-		//        }
-		//        //Log.DebugFormat("QuantityOnHandText: {0}", returnValue);
-		//        return returnValue;
-		//    }
-		//}
-
-		///// <summary>
-		///// 
-		///// </summary>
-		//public string QuantityOnOrderText
-		//{
-		//    get
-		//    {
-		//        string returnValue = null;
-
-		//        //Status	Display
-		//        //ACT	    - QTY OH XXXX
-		//        //          - If qty = less than 0, display 0
-		//        //          - If QTY OH = 0 display new line item: QTY on order from publisher = XXXX	
-		//        //NYP	    - QTY OH = O 
-		//        //          - Add Line Item: QTY on order from publisher = XXXX	
-		//        //Log.DebugFormat("Status.Code: {0}, Status.Id: {1}, QuantityOnHand: {2}", Status.Code, Status.Id, QuantityOnHand);
-		//        //Log.DebugFormat("InStockOverride: {0}, Format: {1}", InStockOverride, Format);
-		//        int quantityOnHand = (QuantityOnHand < 0) ? 0 : QuantityOnHand;
-		//        int quantityOnOrder = (QuantityOnOrder < 0) ? 0 : QuantityOnOrder;
-
-		//        //ProductStatusCodes productStatusCodes = (ProductStatusCodes)Status.Id;
-		//        //Log.DebugFormat("quantityOnHand: {0}, productStatusCodes: {1}", quantityOnHand, productStatusCodes);
-
-		//        if ((!InStockOverride) && (!Format.Equals("LWW Text Package")))
-		//        {
-		//            if (quantityOnHand == 0)
-		//            {
-		//                if ((Status == ProductStatusFactory.Active) || (Status == ProductStatusFactory.NotYetPublished))
-		//                {
-		//                    returnValue = string.Format("{0}", quantityOnOrder);
-		//                }
-		//            }
-		//        }
-
-		//        //Log.DebugFormat("QuantityOnOrderText: {0}", returnValue);
-		//        return returnValue;
-		//    }
-		//}
-
-
-        /// <summary>
-        /// 
-        /// </summary>
-        public string Dimensions
-        {
-            get
-            {
-                if ((Width > 0) && (Width > 0) && (Thickness > 0))
-                {
-                    return string.Format("{0:0.0#} x {1:0.0#} x {2:0.0#} in", Length * 0.01, Thickness * 0.01, Width * 0.01);
-                }
-                return "";
-            }
-        }
-
         /// <summary>
         /// Determines if the product is an R2 product or not.
         /// </summary>
@@ -300,77 +74,6 @@ namespace MarcRecordServiceApp.Core.DataAccess.Entities
                 return (Sku.StartsWith("R2P"));
             }
         }
-
-        /// <summary>
-        /// Default constructor
-        /// </summary>
-        public Product()
-        {
-            Prices = new decimal[6];
-			//Status = new StatusNone();
-			//Publisher = new Publisher();
-			//Category = new Category();
-        }
-
-        /// <summary>
-        /// 
-        /// </summary>
-        /// <returns></returns>
-        public string[] GetBrandonHillDescriptions()
-        {
-            List<string> descriptions = new List<string>();
-            if (string.IsNullOrEmpty(BrandonHillCodes))
-            {
-                return descriptions.ToArray();
-            }
-
-            string[] codes = BrandonHillCodes.Split(',');
-            foreach (string code in codes)
-            {
-                if (code == "M1")
-                {
-                    descriptions.Add("Medical Select Title");
-                    continue;
-                }
-
-                if (code == "M2")
-                {
-                    descriptions.Add("Core Select Title");
-                    continue;
-                }
-
-                if (code == "M3")
-                {
-                    descriptions.Add("Min Select Title");
-                    continue;
-                }
-
-                if (code == "N1")
-                {
-                    descriptions.Add("Nursing Select Title");
-                    continue;
-                }
-
-                if (code == "N2")
-                {
-                    descriptions.Add("Nursing Core Title");
-                    continue;
-                }
-
-                if (code == "A1")
-                {
-                    descriptions.Add("Allied Health Select Title");
-                    continue;
-                }
-
-                if (code == "A2")
-                {
-                    descriptions.Add("Allied Health Core Title");
-                }
-            }
-            return descriptions.ToArray();
-        }
-
     }
 }
 
--- MarcRecordServiceApp/MarcRecordServiceApp/Tasks/TaskBase.cs
+++ MarcRecordServiceApp/MarcRecordServiceApp/Tasks/TaskBase.cs
@@ -11,6 +11,9 @@ namespace MarcRecordServiceApp.Tasks
 {
     public abstract class TaskBase2 : ITask
     {
+        private readonly Random _random = new Random();
+        private readonly DateTime _currentDateTime = DateTime.Now;
+
         protected static readonly ILog Log = LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType.FullName);
 
         protected TaskEmailSettings EmailSettings { get; private set; }
@@ -143,6 +146,109 @@ namespace MarcRecordServiceApp.Tasks
             TaskResult.EndTime = DateTime.Now;
             TaskResultFactory.UpdateTaskResult(TaskResult);
             SendCompleteEmail();
+        }
+
+
+        public string GetRbdMrkFileText(Product product)
+        {
+            try
+            {
+                string publicationYearText = product.PublicationYearText;
+
+                if (publicationYearText == "")
+                {
+                    Log.DebugFormat("Id: {0}, Sku: {1}, Isbn13: {2}", product.Id, product.Sku, product.Isbn13);
+                }
+
+                StringBuilder mrkFileText = new StringBuilder();
+                string sitepath = Settings.Default.SiteSubDirectory;
+
+                mrkFileText.AppendFormat("=LDR  {0}nam  22{1}2a 4500", GetNext5DigitRandomNumber(),
+                                         GetNext5DigitRandomNumber()).AppendLine();
+
+                Log.DebugFormat("PublicationYearText: {0}, PublicationYear: {1}, sku: {2}", product.PublicationYearText,
+                                product.PublicationYear, product.Sku);
+
+
+                string line001 = string.Format("=001  {0}", product.Sku);
+                string line005 = string.Format("=005  {0:yyyyMMddhhmmss}.0", DateTime.Now);
+
+                string line008 =
+                    string.Format("=008  {0:yyMMdd}s{1:0000}\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\eng\\d ",
+                                  _currentDateTime, product.PublicationYear);
+
+                Log.DebugFormat("line008: {0}", line008);
+                Log.DebugFormat("line008.Length: {0}", line008.Length);
+
+                mrkFileText.AppendLine(line001);
+                mrkFileText.AppendLine(line005);
+                mrkFileText.AppendLine(line008);
+
+                //The MRC file requires an additional \\. For some reason it loses when when converted
+                //string extraSlashes = (mrkOnly) ? "" : "\\";
+                const string extraSlashes = "";
+                mrkFileText.AppendFormat("=020  \\\\{1}$a{0}", product.Isbn10, extraSlashes).AppendLine()
+                    .AppendFormat("=020  \\\\{1}$a{0}", product.Isbn13, extraSlashes).AppendLine();
+
+                mrkFileText.AppendLine("=037  \\\\$bRittenhouse Book Distributors, Inc")
+                    .AppendFormat("=100  1\\$a{0}", StripOffCarriageReturnAndLineFeed(product.Authors)).AppendLine()
+                    .AppendFormat("=245  10$a{0}", StripOffCarriageReturnAndLineFeed(product.Title)).AppendLine();
+
+                mrkFileText.AppendFormat("=260  \\\\$b{0},$c{1}", product.PublisherName, publicationYearText).AppendLine
+                    ();
+                mrkFileText.AppendFormat(
+                    "=533  \\\\$a{0}.$bKing of Prussia, PA:$cRittenhouse Book Distributors, Inc,$d{1}", product.Format,
+                    publicationYearText)
+                    .AppendLine();
+
+                mrkFileText.AppendFormat("=650  \\0$a{0}.", product.CategoryName).AppendLine()
+                    .AppendFormat("=700  1\\$a{0}", StripOffCarriageReturnAndLineFeed(product.Authors)).AppendLine()
+                    .AppendFormat("=856  4\\$zConnect to this resource online$u{0}Products/Book.aspx?sku={1}", sitepath,
+                                  product.Isbn10).AppendLine()
+                    .AppendLine();
+
+                return mrkFileText.ToString();
+            }
+            catch (Exception ex)
+            {
+                if (product != null)
+                {
+                    Log.Info(product.ToString());
+                    Log.InfoFormat("Id: {0}", product.Id);
+                    Log.InfoFormat("Sku: {0}", product.Sku);
+                    Log.InfoFormat("Isbn10: {0}", product.Isbn10);
+                    Log.InfoFormat("Isbn13: {0}", product.Isbn13);
+                    Log.InfoFormat("Title: {0}", product.Title);
+                    Log.InfoFormat("Authors: {0}", product.Authors);
+                    Log.InfoFormat("PublicationYearText: {0}", product.PublicationYearText);
+                    Log.InfoFormat("PublicationDate: {0}", product.PublicationDate);
+                    Log.InfoFormat("Copyright: {0}", product.Copyright);
+                    Log.InfoFormat("Format: {0}", product.Format);
+                    Log.InfoFormat("CategoryName: {0}", product.CategoryName);
+                }
+                else
+                {
+                    Log.Info("Product is null!");
+                }
+                Log.Error(ex.Message, ex);
+                throw;
+            }
+
+        }
+
+        private static string StripOffCarriageReturnAndLineFeed(string value)
+        {
+            if (string.IsNullOrEmpty(value))
+            {
+                return value;
+            }
+            return value.Replace("\r", string.Empty).Replace("\n", string.Empty);
+        }
+
+        private int GetNext5DigitRandomNumber()
+        {
+            int next = _random.Next(10000, 99999);
+            return next;
         }
 
     }
