427a929bebbf384477aa23a39e9a06e021d0fd23
KenHaberle <kenhaberle@technotects.com>
2015-08-28 12:55:50 +0000

    Finally got around to organizing the deployment scripts for this project.

--- MarcRecordServiceApp/MarcRecordServiceApp/Tasks/MarcRecords/OclcR2LibraryMarcRecordsTask.cs
+++ MarcRecordServiceApp/MarcRecordServiceApp/Tasks/MarcRecords/OclcR2LibraryMarcRecordsTask.cs
@@ -82,9 +82,11 @@ namespace MarcRecordServiceApp.Tasks.MarcRecords
 
                 mrkFileText.AppendFormat("=LDR  {0}nam  22{1}2a 4500", GetNext5DigitRandomNumber(), GetNext5DigitRandomNumber()).AppendLine();
 
-                mrkFileText.AppendLine(string.Format("=001  {0}", resource.Isbn));
-                mrkFileText.AppendLine(string.Format("=005  {0:yyyyMMddhhmmss}.0", DateTime.Now));
-                mrkFileText.AppendLine(string.Format("=008  {0:yyMMdd}s{1:0000}{2}eng{3}o", _currentDateTime, resource.PublicationYear, GetSpace(23), GetSpace(1)));
+                mrkFileText.AppendFormat("=001  {0}", resource.Isbn).AppendLine();
+                mrkFileText.AppendFormat("=005  {0:yyyyMMddhhmmss}.0", DateTime.Now).AppendLine();
+                mrkFileText.AppendLine("=006  m|||||o||d||||||||");
+                mrkFileText.AppendFormat("=007  cr{0}bn", GetSpace(1)).AppendLine();
+                mrkFileText.AppendFormat("=008  {0:yyMMdd}s{1:0000}{2}xxua{3}o{4}000{5}0{5}eng{5}d", _currentDateTime, resource.PublicationYear, GetSpace(4), GetSpace(4), GetSpace(5), GetSpace(1)).AppendLine();
 
                 var firstAuthorString = resource.AuthorList.Any()
                     ? resource.AuthorList.First(x => x.Order == 1).ToDisplayName()
@@ -107,19 +109,24 @@ namespace MarcRecordServiceApp.Tasks.MarcRecords
 
                 mrkFileText.AppendFormat("=037  {0}$bRittenhouse Book Distributors, Inc", GetSpace(2)).AppendLine();
                 mrkFileText.AppendFormat("=100  1{0}$a{1}", GetSpace(1), firstAuthorString).AppendLine();
-                mrkFileText.AppendFormat("=245  10$a{0}", StripOffCarriageReturnAndLineFeed(resource.Title)).AppendLine();
+
+                mrkFileText.AppendFormat("=245  10$a{0}$h[electronic resource] /$c{1}", StripOffCarriageReturnAndLineFeed(resource.Title), firstAuthorString).AppendLine();
+
                 mrkFileText.AppendFormat("=260  {0}$b{1},$c{2}", GetSpace(2), resource.PublisherName, resource.PublicationYear).AppendLine();
                 mrkFileText.AppendFormat("=300  {0}$a online resource", GetSpace(2)).AppendLine();
                 mrkFileText.AppendFormat("=533  {0}$aeBook.$bKing of Prussia, PA:$cRittenhouse Book Distributors, Inc,$d{1}", GetSpace(2), resource.PublicationYear).AppendLine();
+                mrkFileText.AppendFormat("=530  {0}$aAlso available in print edition.", GetSpace(2)).AppendLine();
                 mrkFileText.AppendFormat("=538  {0}$a Mode of Access: World Wide Web", GetSpace(2)).AppendLine();
-
+                mrkFileText.AppendFormat("=588  {0}$aOnline resource.", GetSpace(2)).AppendLine();
                 WriteCategories(mrkFileText, resource);//650
 
                 mrkFileText.AppendFormat("=655  {0}4$a Electronic books", GetSpace(1)).AppendLine();
 
                 WriteAuthors(mrkFileText, resource);//700
 
-                mrkFileText.AppendFormat("=856  4{0}$zConnect to this resource online$u{1}{2}", GetSpace(1), Settings.Default.R2WebSite, resource.Isbn).AppendLine();
+                mrkFileText.AppendFormat("=776  1{0}$z{1}", GetSpace(1), resource.Isbn).AppendLine();
+
+                mrkFileText.AppendFormat("=856  40$zConnect to this resource online$u{0}{1}", Settings.Default.R2WebSite, resource.Isbn).AppendLine();
 
                 return mrkFileText.ToString();
             }
